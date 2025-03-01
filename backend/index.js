c84a214b9a9ea7a922b398ecc93f90e933d47ad6
Sweta <73096700+nazinegi@users.noreply.github.com>
2025-03-01 13:47:52 +0530

    hu

--- backend/index.js
+++ backend/index.js
@@ -3,10 +3,13 @@ import { connectDB } from "./db/connectDB.js";
 import dotenv from "dotenv";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import path from "path";
 import authRoutes from "./routes/auth.route.js"
+
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
 
 app.use(cors({
@@ -20,6 +23,14 @@ app.use(cookieParser()); // allows us to parse incoming cookies
 
 
 app.use("/api/auth", authRoutes)
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname,"/frontend/dist")));
+
+    app.get("*", (req,res)=>{
+        res.sendFile(path.resolve(__dirname, "frontend", "dist", 'index.html'));
+    })    
+}
 
 app.listen(PORT, ()=>{
     connectDB();
