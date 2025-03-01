81e47ec583dd4555710c324e6d046cdeb84c692a
Sweta <73096700+nazinegi@users.noreply.github.com>
2025-03-01 14:28:52 +0530

    hu

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,9 +2,18 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
-    tailwindcss()
-  ],
+  plugins: [react(), tailwindcss()],
+  build: {
+    chunkSizeWarningLimit: 500, // Set chunk size limit (default is 500KB)
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor'; // Separate vendor dependencies into their own chunk
+          }
+        }
+      }
+    }
+  }
 })
